5743f7daddbd6ecf875a61913c5ae93c5c27f445
opensativo <dmatosconsult@gmail.com>
2020-09-14 07:22:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { FilePond, File, registerPlugin } from "react-filepond";
+import "filepond/dist/filepond.min.css";
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import FilePondPluginMediaPreview from "filepond-plugin-media-preview";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+import "filepond-plugin-media-preview/dist/filepond-plugin-media-preview.css";
+import "./App.css";
+import { createCloudinary } from "./cloudinaryServer";
+
+registerPlugin(
+  FilePondPluginImageExifOrientation,
+  FilePondPluginImagePreview,
+  FilePondPluginMediaPreview,
+  FilePondPluginFileValidateType
+);
 
 function App() {
+  const [state, setState] = useState({
+    images: [],
+    videos: [],
+    body: "",
+    mediaLinks: [],
+  });
+  console.log(state);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="control">
+        <textarea
+          class="textarea has-fixed-size"
+          placeholder="Fixed size textarea"
+        ></textarea>
+      </div>
+      <FilePond
+        files={state.images}
+        allowMultiple={true}
+        acceptedFileTypes={["image/*"]}
+        maxFiles={3}
+        server={createCloudinary("xklusivo", "kzlfjrv5")}
+        name="images"
+        labelIdle='<p><span class="icon is-large">
+                    <span class="fa-lg">
+                      <i class="fas fa-camera fas fa-2x has-text-info"></i>
+                    </span>                    
+                  </span>Adicionar foto</p>'
+        onupdatefiles={(fileItems) => {
+          setState({
+            ...state,
+            images: fileItems.map((fileItem) => fileItem.file),
+          });
+        }}
+      />
+      <FilePond
+        files={state.videos}
+        allowMultiple={true}
+        acceptedFileTypes={["video/*"]}
+        maxFiles={1}
+        server={createCloudinary("xklusivo", "kzlfjrv5")}
+        name="videos"
+        labelIdle='<p><span class="icon is-large">
+                    <span class="fa-lg">
+                      <i class="fas fa-video fas fa-2x has-text-info"></i>
+                    </span>                    
+                  </span>Adicionar video</p>'
+        onupdatefiles={(fileItems) => {
+          setState({
+            ...state,
+            videos: fileItems.map((fileItem) => fileItem.file),
+          });
+        }}
+      />
     </div>
   );
 }
